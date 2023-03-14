ab862630df55543efb1c4b2fdc656f88d048d72c
Sam Hulick <samh@reelcrafter.com>
2023-03-14 12:37:57 -0500

    Initial commit

---
+++ src/components/MyTest.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+/** The top-level await causes event handling to not work (button click) */
+await new Promise((resolve) => setTimeout(resolve, 1000));
+
+function wow() {
+  console.log("wow!!");
+}
+</script>
+
+<template>
+  <div>
+    Hello!
+    <button @click="wow">Go go go</button>
+  </div>
+</template>
